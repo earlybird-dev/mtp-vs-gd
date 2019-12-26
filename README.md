2e3ac60e66978de50a624031be33808c023d371b
Ha Nguyen <hellotrungha@users.noreply.github.com>
2019-12-26 12:19:44 +1100

    Update model

--- README.md
+++ README.md
@@ -8,7 +8,3 @@
 - Fast.ai lesson 1: [https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson1-pets.ipynb](https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson1-pets.ipynb)
 
 - Fast.ai lesson 2: [https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson2-download.ipynb](https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson2-download.ipynb)
-
-### 2. Productize the model
-
-- Deploying on Render: [https://course.fast.ai/deployment_render.html](https://course.fast.ai/deployment_render.html)
--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from fastai.vision import open_image, load_learner
+from fastai.vision import open_image, load_learner, image
 import streamlit as st
+import numpy as np
 import matplotlib.image as mpimg
+from matplotlib.pyplot import imshow
 import os
 import time
+
+import PIL.Image
+import requests
+from io import BytesIO
 
 def main():
     # App title
@@ -41,7 +47,32 @@ def main():
         st.image(display_img, use_column_width=True)
 
     else:
-        pass
+        url = st.text_input("Please input an url:")
+
+        if url != "":
+            try:
+                # Read image from the url
+                response = requests.get(url)
+                import_img = PIL.Image.open(BytesIO(response.content))
+
+                # Transform the image to feed into the model
+                img = import_img.convert('RGB')
+                img = image.pil2tensor(img, np.float32).div_(255)
+                img = image.Image(img)
+
+                # Load model and make
+                model = load_learner('data/train/')
+                pred_class = model.predict(img)[0]
+
+                # Display the prediction
+                if str(pred_class) == 'mtp':
+                    st.success("This is Son Tung MTP from Vietnam!")
+                else:
+                    st.success("This is G-Dragon from Korea.")
+
+                st.image(np.asarray(import_img), use_column_width=True)
+            except:
+                st.text("Invalid url!")
 
 if __name__ == "__main__":
     main()
